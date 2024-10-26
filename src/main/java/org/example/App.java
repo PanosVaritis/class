e2dda68b8d803fbe25fe15c1db7b33acdc414247
Panagiotis Varitis <panosvaritis@icloud.com>
2024-10-26 19:49:07 +0300

    New changes occured in the interface

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,18 +3,8 @@ package org.example;
 public class App {
 
     public static void main (String[] args){        
-        
-        LightWithInt livingRoomLight = new LightWithInt (200);
-        
-        Dimmable bedroomLight = new LightWithInt (500);
-        
-        Switchable bathroomLight = new LightWithInt (1000);
-        
-        Switchable light1;
-        Dimmable light2;
-        LightWithInt light3;
+   
         
         
-       
     }
 }
